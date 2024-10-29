61e3ae58bcf55cf862b8ea6294f4ff09a5e6398a
EmiTrack999 <sancheremiliano9@gmail.com>
2024-10-28 23:44:20 -0600

    Nuevas Vistas

--- Proyecto/Proyecto/src/Vistas/Viaje.java
+++ Proyecto/Proyecto/src/Vistas/Viaje.java
@@ -6,11 +6,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Toolkit;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
 
 public class Viaje extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -40,6 +48,55 @@ public class Viaje extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblIniciemosUnNuevo = new JLabel("Iniciemos un nuevo viaje ");
+		lblIniciemosUnNuevo.setForeground(Color.BLACK);
+		lblIniciemosUnNuevo.setFont(new Font("MS UI Gothic", Font.BOLD, 20));
+		lblIniciemosUnNuevo.setBounds(305, 10, 252, 37);
+		contentPane.add(lblIniciemosUnNuevo);
+		
+		JLabel lblLocalidadDondeSe = new JLabel("Localidad donde se lleva el Producto");
+		lblLocalidadDondeSe.setForeground(Color.BLACK);
+		lblLocalidadDondeSe.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblLocalidadDondeSe.setBounds(275, 57, 282, 19);
+		contentPane.add(lblLocalidadDondeSe);
+		
+		textField = new JTextField();
+		textField.setBounds(275, 86, 282, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblelProductoEs = new JLabel("¿El Producto es Fragil?");
+		lblelProductoEs.setForeground(Color.BLACK);
+		lblelProductoEs.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblelProductoEs.setBounds(325, 115, 185, 19);
+		contentPane.add(lblelProductoEs);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Si");
+		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.BOLD, 15));
+		rdbtnNewRadioButton.setBounds(278, 140, 103, 21);
+		contentPane.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("No");
+		rdbtnNewRadioButton_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		rdbtnNewRadioButton_1.setBounds(454, 140, 103, 21);
+		contentPane.add(rdbtnNewRadioButton_1);
+		
+		JLabel lblLugarDondeSe = new JLabel("Lugar donde Se llevara Acabo el Viaje");
+		lblLugarDondeSe.setForeground(Color.BLACK);
+		lblLugarDondeSe.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblLugarDondeSe.setBounds(275, 173, 306, 19);
+		contentPane.add(lblLugarDondeSe);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(275, 202, 282, 19);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Finalizar");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton.setBounds(371, 344, 139, 45);
+		contentPane.add(btnNewButton);
 	}
 
 }
