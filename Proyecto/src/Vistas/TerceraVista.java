329c23a2ffac8e10b8cb29280c68fc90ede89117
vladimir754 <vladialex1c@gmail.com>
2024-11-05 16:26:50 -0600

    Vistas, TXTFiel, Update

--- Proyecto/src/Vistas/TerceraVista.java
+++ Proyecto/src/Vistas/TerceraVista.java
@@ -5,6 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import Controlador.B_Datos;
+import Controlador.Modelo;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.Color;
@@ -13,15 +17,19 @@ import java.awt.Font;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TerceraVista extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtTelefono;
+	private JTextField txtContra;
 	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtConfir;
 	private JLabel lblNewLabel_3;
 	private JButton btnNewButton;
 	private JMenuBar menuBar;
@@ -88,25 +96,25 @@ public class TerceraVista extends JFrame {
 		lblNewLabel_2.setBounds(261, 235, 149, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		textField = new JTextField();
-		textField.setBounds(219, 98, 250, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtTelefono = new JTextField();
+		txtTelefono.setBounds(219, 98, 250, 20);
+		contentPane.add(txtTelefono);
+		txtTelefono.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(219, 184, 250, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		txtContra = new JTextField();
+		txtContra.setBounds(219, 184, 250, 20);
+		contentPane.add(txtContra);
+		txtContra.setColumns(10);
 		
 		textField_2 = new JTextField();
 		textField_2.setBounds(1000, 1000, 1000, 1000);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(219, 260, 250, 20);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		txtConfir = new JTextField();
+		txtConfir.setBounds(219, 260, 250, 20);
+		contentPane.add(txtConfir);
+		txtConfir.setColumns(10);
 		
 		lblNewLabel_3 = new JLabel("Registro");
 		lblNewLabel_3.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 40));
@@ -115,9 +123,56 @@ public class TerceraVista extends JFrame {
 		contentPane.add(lblNewLabel_3);
 		
 		btnNewButton = new JButton("Registrarme");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Modelo db = new Modelo();
+				String contraseña = txtContra.getText();
+				String confirmarContraseña = txtConfir.getText();
+				String telefono = txtTelefono.getText();
+				Inicio in = new Inicio();
+				B_Datos bd=new B_Datos();
+				if (telefono.isEmpty()) {
+				    JOptionPane.showMessageDialog(null, "Ingresa tu correo");
+				  
+				} else if (!db.validarCorreo(telefono)) {
+				    JOptionPane.showMessageDialog(null, "Correo inválido");
+				} else if (contraseña.isEmpty()) {
+				    JOptionPane.showMessageDialog(null, "Ingresa tu contraseña");
+				} else if (confirmarContraseña.isEmpty()) {
+				    JOptionPane.showMessageDialog(null, "Confirma tu contraseña");
+				} else if (!contraseña.equals(confirmarContraseña)) {
+				    JOptionPane.showMessageDialog(null, "Las contraseñas no coinciden");
+				 
+				 } else {
+					 registrar();
+				 
+				}
+				
+			}
+		});
 		btnNewButton.setForeground(new Color(255, 255, 255));
 		btnNewButton.setBackground(new Color(255, 0, 0));
 		btnNewButton.setBounds(293, 306, 111, 14);
 		contentPane.add(btnNewButton);
+	}
+	public void registrar() {
+		B_Datos bd=new B_Datos();
+		Inicio in=new Inicio();
+		String telefono = txtTelefono.getText();
+	    String contraseña = txtContra.getText();
+	    String confirmar = txtConfir.getText();
+	    if (bd.verificarCorreo(telefono)) {
+	        JOptionPane.showMessageDialog(null, "El correo ya está registrado.");
+	        return;
+	    }
+	    Modelo mo = new Modelo();
+	    mo.setCorreo(telefono);
+	    mo.setContraseña(contraseña);
+	    if (bd.registrarse(mo)) {
+	        JOptionPane.showMessageDialog(null, "Bienvenido");
+	        in.setVisible(true);
+	    } else {
+	        JOptionPane.showMessageDialog(null, "Error al registrar.");
+	    }
 	}
 }
